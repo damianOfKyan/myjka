12435b5e83f98ce1d3582f731c99a949657b884b
Damian Boni <damian@kyan.com>
2021-10-07 21:54:03 +0100

    better print quality

--- resources/js/Pages/Certificates/Print.vue
+++ resources/js/Pages/Certificates/Print.vue
@@ -272,8 +272,9 @@ export default {
       const opt = {
         margin: 0,
         filename: 'myfile.pdf',
-        image: { type: 'jpeg', quality: 0.98 },
-        html2canvas: { scale: 1 },
+        // image: { type: 'jpeg', quality: 0.98 },
+        image: { type: 'png' },
+        html2canvas: { scale: 2 },
         jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' },
       }
       html2pdf.default( element, opt )
